09bcd7e3675be15993ee98fbc28f477e59673acd
Nelson Flamenco <Nelson Flamenco@Hp-PC>
2018-05-02 20:40:37 -0600

    se modifico el motor completo del sistema

--- src/com/classes/Engine.java
+++ src/com/classes/Engine.java
@@ -8,12 +8,12 @@ package com.classes;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
+/*
  * @author allan
  * la clase Engine es el motor principal de la aplicacion, se inicializa desde el main para poder correr
  * el sistema y la solucion
- */
+*/
+
 public class Engine {
     private boolean status = false; //Bandera que indica si el programa se ha inicializado su default es falso y se tiene que poner a true para que ejecute
     private String operation; //Tipo de operacion realizada
@@ -23,39 +23,61 @@ public class Engine {
     /*
     * initSystem inicializa el sistema, es necesario ejecutarlo despues de la declaracion de un objeto de Clase Engine.
     */
+    
     public void initSystem(){
         this.setStatus(true);
-        
+        System.out.println("*****************BIENVENIDO***************");
         while (this.isStatus()){
-            System.out.println("*****HOTEL VIÑA RAFINHA*****");
-            System.out.println("**********BIENVENIDO********");
-            System.out.println("Seleccione la operacion que desea hacer:");
-            System.out.println("1 - Administracion del hotel.");
-            System.out.println("2 - Administracion de clientes.");
-            System.out.println("3 - Administracion de reservaciones");
-            System.out.println("4 - Administracion de ventas");
-            System.out.println("5 - Cerrar sistema.");
+            System.out.println("");
+            System.out.println("************HOTEL VIÑA RAFINHA************");
+            System.out.println("Seleccione en el menú la opcion que desea:");
+            System.out.println("1 - Control de Hotel"               );
+            System.out.println("2 - Reservaciones"                  );
+            System.out.println("3 - Gestor de Clientes"            );
+            System.out.println("4 - Manejo de Ventas y Servicios"   );
+            System.out.println("5 - Apagar Sistema..."              );
             
+            System.out.println("opcion:");
             //obteniendo valor de operacion
             operation = in.nextLine(); 
             
             //validando valor de operacion
             switch (operation){
                 case "1":
-                    System.out.println("INGRESANDO A MODULO DE ADMINISTRACION DE HOTEL");
+                    System.out.println("");
+                    System.out.println("**************CONTROL DE HOTEL**************" );
+                    this.controlHotel();
+                    break;
+                    
+                case "2":
+                    System.out.println("");
+                    System.out.println("***************RESERVACIONES***************"  );
+                    this.reservaHotel();
+                    break;  
+                    
+                case "3":
+                    System.out.println("");
+                    System.out.println("*************GESTOR DE CLIENTES*************" );
+                    this.clienteHotel();
+                    break;
+                    
+                case "4":
+                    System.out.println("");
+                    System.out.println("********MANEJO DE VENTAS Y SERVICIOS********" );
                     this.adminHotel();
                     break;
-                
+                    
                 case "5":
-                    System.out.println("Adios!");
+                    System.out.println("");
+                    System.out.println("*********APAGANDO SISTEMA DE HOTEL*********"  );
+                    System.out.println("Adios!"                                       );
                     this.setStatus(false);
                     break;
                     
                 default: 
-                    System.out.println("Favor ingrese una operacion valida");
+                    System.out.println("Favor ingrese una operacion valida..."        );
                     break;
             }
-            
         }
     }
     
@@ -82,13 +104,14 @@ public class Engine {
     pisos: cantidad de pisos que tendrá el hotel; 
     habitaciones: Cantidad de pisos por habitacion;
     */
+    
     public void initSettings(int pisos, int habitaciones){
         ArrayList<Habitacion> rooms;
-        System.out.println("Inicializando configuracion de pisos");
-        System.out.println(Character.toChars('A'+pisos));
+        //System.out.println("Inicializando configuracion de pisos");
+        //System.out.println(Character.toChars('A'+pisos));
         //creando el piso
         for (char alphabet = 'A'; alphabet <= 'A'+pisos; alphabet++) {
-            System.out.println(Character.toString(alphabet));
+            //System.out.println(Character.toString(alphabet));
             rooms = new ArrayList();
             Piso newpiso = new Piso();
             newpiso.setCodPiso(Character.toString(alphabet));
@@ -124,8 +147,10 @@ public class Engine {
     retorna la ubicacion del piso que se quiere buscar
     retorna -1 si no se encuentra nada en el ArrayList
     */
+    
     private int buscarPisoPorCodigo(String codigo){
-        int pisoLocation = -1; //-1 es un valor por defecto, indica que no se encontró el piso solicitado
+        int pisoLocation = -1;
+        //-1 es un valor por defecto, indica que no se encontró el piso solicitado
         //buscando a traves del arraylist el codigo del piso
         for(int i = 0; i < pisosHotel.size(); i++){
             Piso p = pisosHotel.get(i);
@@ -135,7 +160,6 @@ public class Engine {
                 pisoLocation = i;
             }
         }
-        
         return pisoLocation;
     }
     
@@ -145,55 +169,57 @@ public class Engine {
     es privado ya que solo la clase Engine debe accederla
     de lo contrario funcionaría de manera incompleta
     */
-    private void adminHotel(){
+    
+    private void controlHotel(){
         String option;
         String pisoOpt;
-        boolean admHotelStats = true;
+        boolean controlHotelStats = true;
         
-        while (admHotelStats){
-            System.out.println("");
-            System.out.println("");
-            System.out.println("*****HOTEL VIÑA RAFINHA*****");
-            System.out.println("**ADMINISTRACION DEL HOTEL**");
-            System.out.println("Seleccione la operacion que desea hacer:");
-            System.out.println("1 - Revisar estado de pisos.");
-            System.out.println("2 - Administracion de habitaciones.");
-            System.out.println("3 - Habilitar/desabilitar pisos");
-            System.out.println("4 - Regresar");
+        while (controlHotelStats){
+            System.out.println("Seleccione la operacion que desea realizar:");
+            System.out.println("1 - Revisar estado de pisos."            );
+            System.out.println("2 - Administrador de habitaciones."     );
+            System.out.println("3 - Habilitar/Deshabilitar pisos"        );
+            System.out.println("4 - Regresar..."                         );
+            
+            System.out.println("opcion:");
             
             option = in.nextLine();
             
             switch (option) {
                 case "1":
+                    System.out.println("*************Revisar estado de pisos*************"     );
                     //System.out.println(pisosHotel.size());
                     for(int i = 0; i < pisosHotel.size(); i++){
                         Piso p = pisosHotel.get(i);
-                        System.out.println("PISO " + p.getCodPiso() + ": " + p.isHabilitado());
+                        System.out.println("Piso " + p.getCodPiso() + ": " + p.isHabilitado());
                     }
                     break;
+                    
                 case "2":
+                    System.out.println("**********Administrador de habitaciones**********"     );
                     System.out.println("Ingrese el codigo del piso: ");
                     pisoOpt = in.nextLine();
                                    
                     int pisoLocation = this.buscarPisoPorCodigo(pisoOpt);
                     
-                    //validando si el piso inresado existe
+                    //validando si el piso ingresado existe
                     if(pisoLocation > -1){
                         Piso p = pisosHotel.get(pisoLocation);
                         p.adminHabitaciones();
                     }else{
                         System.out.println("No se ha encontrado el piso ingresado");
                     }
-                    
                     break;
                     
                 case "3":
+                    System.out.println("**********Habilitar/Deshabilitar pisos**********"      );
                     System.out.println("Ingrese el codigo del piso: ");
                     pisoOpt = in.nextLine();
                                    
                     int location = this.buscarPisoPorCodigo(pisoOpt);
                     
-                    //validando si el piso inresado existe
+                    //validando si el piso ingresado existe
                     if(location > -1){
                         Piso p = pisosHotel.get(location);
                         System.out.println("El estado actual del piso es : " + p.isHabilitado() + " , Desea cambiarlo?" );
@@ -204,23 +230,188 @@ public class Engine {
                             pisosHotel.remove(location);
                             //se agrega el nuevo piso para hacer la actualizacion
                             pisosHotel.add(location,p);
+                            System.out.println("Estado de piso modificado...");
                         }
                     }else{
                         System.out.println("No se ha encontrado el piso ingresado");
                     }
                     
                 case "4":
-                    admHotelStats = false;
+                    controlHotelStats = false;
                     break;
+                    
                 default:
                     System.out.println("Favor ingrese una opcion valida");
                     break;
-            }
+            }//fin swicth(controlHotelStats)
+        }//fin while (controlHotelStats)
+    }  
+    
+    private void ingresoCliente(){
+        String nombre,apellido,sexo,email;
+        int edad,telefono,numtarjeta;
+        System.out.println("Ingresando datos personales...");
+        System.out.println("Ingrese nombre:");
+        nombre=in.nextLine();
+        System.out.println("Ingrese apellido:");
+        apellido=in.nextLine();
+        System.out.println("Ingrese edad:");
+        edad=in.nextInt();
+        sexo=in.nextLine();
+        System.out.println("Ingrese sexo:");
+        sexo=in.nextLine();
+        System.out.println("Ingrese telefono:");
+        telefono=in.nextInt();
+        sexo=in.nextLine();
+        System.out.println("Ingrese e-mail:");
+        email=in.nextLine();
+        System.out.println("Ingrese numero tarjeta:");
+        numtarjeta=in.nextInt();
+        System.out.println("Datos ingresados exitosamente...");
+        System.out.println("");
+    } 
+    
+    private void reservaHotel(){
+        String option;
+        boolean reservaHotelStats = true;
+        
+        while (reservaHotelStats){
+            System.out.println("Seleccione la operacion que desea realizar:"    );
+            System.out.println("1 - Realizar reservación"                       );
+            System.out.println("2 - Modificar reservación"                      );
+            System.out.println("3 - Cancelar reservación"                       );
+            System.out.println("4 - Regresar..."                                );
             
-        }//fin while (admHotelStats)
+            System.out.println("opcion:");
+            
+            option = in.nextLine();
+            
+            switch (option) {
+                case "1":
+                    System.out.println("**********Realizar reservacion**********"  );
+                    this.ingresoCliente();
+                    System.out.println("Seleccionando habitación del cliente..."   );
+                    System.out.println("Clase de habitacion a hospedarse:"         );
+                    System.out.println("  1.Sencilla         2.Doble  "            );
+                    
+                    System.out.println("Paquete de Habitacion:");
+                    System.out.println("  1.Agregar           2.Omitir"            );
+                   
+                    System.out.println("Seleccion de Paquete:");
+                    System.out.println("  1.Basico           2.Premium"            );
+                    
+                    System.out.println("Dias de hospedaje:");
+                    
+                    System.out.println("Precio por dia de hospedaje:");
+                    
+                    System.out.println("Costo total:");
+                    break;
+                    
+                case "2":
+                    System.out.println("**********Modificar reservacion**********" );
+                    break;
+                    
+                case "3":
+                    System.out.println("**********Cancelar reservacion**********"  );
+                    break;
+                    
+                case "4":
+                    reservaHotelStats = false;
+                    break;
+                    
+                default:
+                    System.out.println("Favor ingrese una opcion valida");
+                    break;
+            }//fin swicth(reservaHotelStats)
+        }//fin while (reservaHotelStats)
+    }  
+    
+    private void clienteHotel(){
+        String option;
+        boolean clienteHotelStats = true;
+        
+        System.out.println("Seleccione la operacion que desea hacer:");
+        System.out.println("1 - Agregar cliente "                 );
+        System.out.println("2 - Modificar cliente"                );
+        System.out.println("3 - Eliminar cliente "                );
+        System.out.println("4 - Regresar..."                      );
+            
+        System.out.println("opcion:");        
+        
+        while (clienteHotelStats){
+            System.out.println("");
+            
+            option = in.nextLine();
+            
+            switch (option) {
+                case "1":
+                    System.out.println("***********Agregar cliente***********");
+                    this.ingresoCliente();
+                    break;
+                    
+                case "2":
+                    System.out.println("**********Modificar cliente**********");
+                    break;   
+                    
+                case "3":
+                    System.out.println("***********Cancelar cliente**********");
+                    break;   
+                    
+                case "4":
+                    clienteHotelStats = false;
+                    break;
+                    
+                default:
+                    System.out.println("Favor ingrese una opcion valida"      );
+                    break;
+            }//fin swicth(clienteHotelStats)
+        }//fin while (clienteStats)
+    } 
+    
+    private void adminHotel(){
+        String option;
+        boolean adminHotelStats = true;
+        
+        System.out.println("Seleccione la operacion que desea hacer:");
+        System.out.println("1 - Gestion de Precios "                 );
+        System.out.println("2 - Administracion de Servicios"         );
+        System.out.println("3 - Regresar..."                         );
+            
+        System.out.println("opcion:"); 
+        
+        while(adminHotelStats){
+            System.out.println("");
 
-    }
-    
-    
-    
+            option = in.nextLine();
+            
+            switch (option) {
+                case "1":
+                    System.out.println("***********Gestion de Precios***********");
+                    System.out.println("Modificar precio por:"                   );
+                    System.out.println("  1.Habitacion      2.Paquete       "    );
+                    break;
+                    
+                case "2":
+                    System.out.println("******Administracion de Servicios*******");
+                    System.out.println("Seleccion de Paquete:");
+                    System.out.println("  1.Basico           2.Premium"          );
+                    
+                    System.out.println("Modificar Servicios:"                    );
+                    System.out.println("Internet Ilimitado"                      );
+                    System.out.println("Servicio a la Habitacion"                );
+                    System.out.println("Acceso a la piscina"                     );
+                    System.out.println("Acceso al buffet desayuno"               );
+                    System.out.println("Acceso al minibar"                       );
+                    break;  
+                    
+                case "3":
+                    adminHotelStats = false;
+                    break;
+                    
+                default:
+                    System.out.println("Favor ingrese una opcion valida");
+                    break;
+            }//fin swicth(adminHotelStats)
+        }//fin while (adminHotelStats)
+    } 
 }
